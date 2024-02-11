a9fafe9cdaf875ea9400eb705cf3c7530eb50c81
Alex Zalo <zalo.alex56@gmail.com>
2024-02-11 19:25:50 +0100

    encryption button

--- src/content.js
+++ src/content.js
@@ -1,3 +1,21 @@
+function addUMESButton(hoverToolsElement, id) {
+    var UMESButton = document.createElement("img")
+    UMESButton.src = browser.extension.getURL("assets/umes-icon.png")
+    UMESButton.style = "height: 25px; opacity: 1; cursor: pointer"
+
+    UMESButton.addEventListener("click", () => {
+        var message = document.querySelector(`span[umes--id="${id}"]`)
+        var newState = !(message.getAttribute("umes--decrypted") === 'true')
+
+        message.textContent = message.getAttribute(newState ? "umes--decrypted-string" : "umes--original-string")
+        UMESButton.style.opacity = newState ? "1" : "0.5"
+
+        message.setAttribute("umes--decrypted", newState)
+    })
+
+    hoverToolsElement.appendChild(UMESButton)
+}
+
 (async () => {
     await import(chrome.extension.getURL('web-ext-library/content.js'))
 
@@ -5,9 +23,19 @@
 
     console.log(UMES)
 
-    UMES.setMessageContainer(".n5hs2j7m", "div[role='row'] .selectable-text span", (message) => {
-        console.log("[UMES]", message)
-        
+    UMES.setMessageContainer(".n5hs2j7m", ".CzM4m", (node) => {
+        console.log("[UMES] node:", node)
+    
+        if (node == null) {
+            return
+        }
+
+        var message = node.querySelector(".selectable-text span")
+        var messageHoverTools = node.querySelector(".FxqSn")
+
+        console.log("[UMES] message:", message)
+        console.log("[UMES] messageHoverTools:", messageHoverTools)
+
         if (message == null) {
             return
         }
@@ -15,8 +43,18 @@
         var content = message.textContent
         
         if (UMES.isUMESMessage(content)) {
+
+            message.setAttribute("umes--original-string", message.textContent)
+
             UMES.decryptMessage(content, (decrypted) => {
                 message.textContent = decrypted
+
+                var id = Date.now() + Math.random()
+
+                message.setAttribute("umes--decrypted-string", message.textContent)
+                message.setAttribute("umes--decrypted", true)
+                message.setAttribute("umes--id", id)
+                addUMESButton(messageHoverTools, id)
             })
         }
     })
